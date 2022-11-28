f2b6a27fc4712b809b4ed8c13ce1403c5eae4b46
burakS-cloud <bsurumcuoglu@gmail.com>
2022-11-29 01:17:45 +0300

    added port env variable to server code

--- index.js
+++ index.js
@@ -263,6 +263,8 @@ app.post("/api/deleteQR", async (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001!");
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}!`);
 });
